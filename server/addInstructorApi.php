c96405b97e470c31f7674f4346351bbb651b8a40
fh95-tech <fakih.hussein995@gmail.com>
2024-11-18 23:25:50 +0200

    <server>added addInstructor api

---
+++ server/addInstructorApi.php
@@ -0,0 +1,27 @@
+<?php
+include"connection.php";
+
+$username=$_POST["username"];
+$password=$_POST["password"];
+
+$hashed = password_hash($password,PASSWORD_DEFAULT);
+
+$user_type_id=2;
+$query=$connection->prepare("INSERT INTO users(username,password,user_type_id) VALUES (?,?,?)");
+$query->bind_param("ssi",$username,$hashed,$user_type_id);
+$query->execute();
+
+$result = $query->affected_rows;
+if($result != 0){
+    echo json_encode([
+        "status"=>"Successful",
+        "message"=>"$result instructor added"
+    ]);
+}else{
+    echo json_encode([
+        "status"=>"Failed",
+        "message"=>"Could not add the instructor"
+    ]);
+}
+
+?>
