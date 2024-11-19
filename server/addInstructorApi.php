a00e86de01444cd19f3a4f6abf1ae57cceedc1f9
fh95-tech <fakih.hussein995@gmail.com>
2024-11-19 07:41:28 +0200

    <server> added login api

--- server/addInstructorApi.php
+++ server/addInstructorApi.php
@@ -4,11 +4,12 @@ include"connection.php";
 $username=$_POST["username"];
 $password=$_POST["password"];
 
-$hashed = password_hash($password,PASSWORD_DEFAULT);
+// $hashed = password_hash($password,PASSWORD_DEFAULT);
 
 $user_type_id=2;
 $query=$connection->prepare("INSERT INTO users(username,password,user_type_id) VALUES (?,?,?)");
-$query->bind_param("ssi",$username,$hashed,$user_type_id);
+// $query->bind_param("ssi",$username,$hashed,$user_type_id);
+$query->bind_param("ssi",$username,$password,$user_type_id);
 $query->execute();
 
 $result = $query->affected_rows;
