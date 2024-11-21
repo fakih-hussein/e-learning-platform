0af88e60e972dc54352bb6152798d3f2f65924f7
fh95-tech <fakih.hussein995@gmail.com>
2024-11-21 02:10:55 +0200

    <front> created admin Panel

---
+++ src/pages/adminPanel.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import StudentsTable from "./../components/studentsTable.jsx";
+// import InstructorsTable from "./InstructorsTable";
+// import CoursesTable from "./CoursesTable";
+import "../styles/AdminPanel.css";
+
+const AdminPanel = () => {
+  const [activeTab, setActiveTab] = useState("students");
+
+  return (
+    <div className="container">
+      <h1 className="header">Admin Panel</h1>
+      <div className="navbar">
+        <button className="button" onClick={() => setActiveTab("students")}>
+          Students
+        </button>
+        {/* <button className="button" onClick={() => setActiveTab("instructors")}>
+          Instructors
+        </button>
+        <button className="button" onClick={() => setActiveTab("courses")}>
+          Courses
+        </button> */}
+      </div>
+      <div className="content">
+        {activeTab === "students" && <StudentsTable />}
+        {/* {activeTab === "instructors" && <InstructorsTable />}
+        {activeTab === "courses" && <CoursesTable />} */}
+      </div>
+    </div>
+  );
+};
+
+export default AdminPanel;
